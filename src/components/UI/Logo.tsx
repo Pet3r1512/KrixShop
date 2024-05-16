1899429b5bd572fa64118c5676b29fa31a55e87c
Thanh Phong <pttp15122002@gmail.com>
2024-05-16 10:52:24 +0700

    add skeleton for loading

--- src/components/UI/Logo.tsx
+++ src/components/UI/Logo.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
+import { useState } from "react";
+import { Skeleton } from "./ui/skeleton";
+import { cn } from "@/lib/utils";
 
 export default function Logo(props: any) {
+  const [loading, setLoading] = useState(true);
   return (
     <>
       <Image
+        onLoad={() => {
+          setLoading(false);
+        }}
         src={"/images/LogoText.png"}
         alt=""
         width={1000}
@@ -13,6 +20,9 @@ export default function Logo(props: any) {
         {...props}
       />
       <Image
+        onLoad={() => {
+          setLoading(false);
+        }}
         src={"/images/Logo.png"}
         alt=""
         width={1000}
@@ -20,6 +30,12 @@ export default function Logo(props: any) {
         priority
         className="h-[48px] w-auto lg:hidden"
         {...props}
+      />
+      <Skeleton
+        className={cn(
+          "w-[45.81px] h-[64px] lg:w-[155.11px] lg:h-[64px]",
+          loading ? "block" : "hidden"
+        )}
       />
     </>
   );
