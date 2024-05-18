1b89818e10021e60717743ce75669e04ddb567a8
Thanh Phong <pttp15122002@gmail.com>
2024-05-18 20:15:30 +0700

    Add supabase config

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,18 @@ const { i18n } = pkg;
 const nextConfig = {
   reactStrictMode: true,
   i18n,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jlehnhviqykpbhjqjzmp.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/sign/KristShop/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
+
+// https://jlehnhviqykpbhjqjzmp.supabase.co/storage/v1/object/sign/KristShop/Categories/footwear.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJLcmlzdFNob3AvQ2F0ZWdvcmllcy9mb290d2Vhci5qcGciLCJpYXQiOjE3MTYwMzgwMTcsImV4cCI6MTc0NzU3NDAxN30.O7Jxs_LY3ic1d6izVHbWWRcFImxxmrYrzrC4fRuzFuo&t=2024-05-18T13%3A13%3A31.005Z
