ec4fc320ca740023546344713161ac0bb030d9b4
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-05-18 21:05:04 +0700

    Setup xata db (#5)
    * install xata and prisma
    
    * add Category schema
    
    * seed file
    
    * try using Category component

---
+++ src/components/Home/Categories.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Categories() {
+  return (
+    <Image
+      src="https://jlehnhviqykpbhjqjzmp.supabase.co/storage/v1/object/sign/KristShop/Categories/footwear.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJLcmlzdFNob3AvQ2F0ZWdvcmllcy9mb290d2Vhci5qcGciLCJpYXQiOjE3MTYwMzgwMTcsImV4cCI6MTc0NzU3NDAxN30.O7Jxs_LY3ic1d6izVHbWWRcFImxxmrYrzrC4fRuzFuo&t=2024-05-18T13%3A13%3A31.005Z"
+      alt=""
+      width={1000}
+      height={1000}
+    />
+  );
+}
