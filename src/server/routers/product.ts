dd5e36489690cde8aebae89ff23e5da108b106a7
Thanh Phong <pttp15122002@gmail.com>
2024-05-20 16:18:44 +0700

    tRPC setup

--- src/server/routers/product.ts
+++ src/server/routers/product.ts
@@ -1,9 +1,23 @@
-import { z } from "zod";
 import { publicProcedure, router } from "../tRPC";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const productRouter = router({});
-// export type definition of API
+export const productRouter = router({
+  getCategoriesProducts: publicProcedure.query(async () => {
+    prisma.$connect();
+    const products = await prisma.categories.findMany({
+      select: {
+        id: true,
+        name: true,
+        image: true,
+      },
+    });
+    prisma.$disconnect();
+    if (products) {
+      return { products: products };
+    }
+  }),
+});
+
 export type AppRouter = typeof productRouter;
