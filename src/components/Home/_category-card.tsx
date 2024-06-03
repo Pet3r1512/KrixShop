81555f085efab0fe2e712d367f503b15c82e82fd
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-06-03 11:51:03 +0700

    Build link query for sidebar (#16)
    * refactor: add keyprop and use query params for Link
    
    * refactor: handling string and add params to Link
    
    * style: truncating text if too long
    
    * style: change text

--- src/components/Home/_category-card.tsx
+++ src/components/Home/_category-card.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { CardBody, CardContainer, CardItem } from "../UI/ui/aceternity/3d-card";
 import Image from "next/image";
 
@@ -10,11 +11,8 @@ export default function CategoryCard({
   image: string;
   id: number;
 }) {
-  let product_name = !name.split("-")[1]
-    ? name.split("-")[0]
-    : name.split("-")[1];
   return (
-    <CardContainer className="inter-var">
+    <CardContainer key={id} className="inter-var">
       <CardBody className="bg-gray relative group/card dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] h-[300px] lg:h-[500px] max-w-[185px] w-auto lg:max-w-[350px] rounded-xl border overflow-hidden">
         <p className="font-bold text-primary absolute md:top-5 lg:top-10 left-[80px] text-3xl lg:text-[120px] opacity-60">
           KRIST
@@ -25,13 +23,19 @@ export default function CategoryCard({
             height={1000}
             width={1000}
             quality={50}
+            priority
             className="w-auto mx-auto min-h-full object-cover rounded-xl"
             alt="thumbnail"
           />
         </CardItem>
-        <button className="absolute bottom-4 lg:bottom-6 left-1/2 -translate-x-1/2 w-4/5 bg-white text-black font-semibold lg:font-bold text-base lg:text-2xl shadow-2xl px-2 pb-1.5 pt-1 lg:px-2.5 lg:pb-2 lg:pt-1.5 rounded-lg">
+        <Link
+          href={`/shop/${name.toLowerCase().split("-")[0]}/${
+            name.toLowerCase().split("-")[1]
+          }`}
+          className="flex justify-center absolute bottom-4 lg:bottom-6 left-1/2 -translate-x-1/2 w-4/5 bg-white text-black font-semibold lg:font-bold text-base lg:text-2xl shadow-2xl px-2 pb-1.5 pt-1 lg:px-2.5 lg:pb-2 lg:pt-1.5 rounded-lg truncate"
+        >
           {name}
-        </button>
+        </Link>
       </CardBody>
     </CardContainer>
   );
