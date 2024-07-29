e13ed24e273adbd72c110a9b7ec8c4be08f683fd
Thanh Phong <pttp15122002@gmail.com>
2024-07-30 09:06:32 +1000

    Feat: display clicked product information

---
+++ src/pages/shop/item/[...slug].tsx
@@ -0,0 +1,31 @@
+import Layout from "@/components/UI/Layout";
+import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useState } from "react";
+import { trpc } from "@/server/utils/tRPC";
+import ProductCard from "@/components/Shop/Product-card";
+
+export default function ItemDetail() {
+  const [product, setProduct] = useState();
+  const router = useRouter();
+
+  const productQuery = trpc.product.getProductById.useQuery({
+    xid: router.asPath.split("/")[3].toString(),
+  });
+
+  useEffect(() => {
+    if (productQuery.isSuccess) {
+      setProduct(productQuery.data.item!);
+    }
+    console.log(product);
+  }, [productQuery]);
+
+  return <Layout>{product ? <ProductCard product={product} /> : <></>}</Layout>;
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale as string, ["common"])),
+  },
+});
