c6254e628f6336f166dcc81d92283c40eddcbbfc
Thanh Phong <pttp15122002@gmail.com>
2024-07-30 20:23:08 +1000

    Feat: (cont) build product detail page

--- src/pages/shop/item/[...slug].tsx
+++ src/pages/shop/item/[...slug].tsx
@@ -2,27 +2,41 @@ import Layout from "@/components/UI/Layout";
 import { useRouter } from "next/router";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
-import { useEffect, useState } from "react";
 import { trpc } from "@/server/utils/tRPC";
-import ProductCard from "@/components/Shop/Product-card";
+import Image from "next/image";
 
 export default function ItemDetail() {
-  const [product, setProduct] = useState<any>();
   const router = useRouter();
 
   const productQuery = trpc.product.getProductById.useQuery({
     xid: router.asPath.split("/")[3].toString(),
   });
 
-  useEffect(() => {
-    if (productQuery.isSuccess) {
-      setProduct(productQuery.data.item!);
-    }
-  }, [productQuery]);
-
   return (
     <Layout pageName="Shop">
-      {product ? <ProductCard key={product.id} product={product} /> : <></>}
+      {!productQuery.isLoading &&
+      productQuery.isSuccess &&
+      productQuery.data ? (
+        <section className="lg:my-16 lg:flex lg:flex-row lg:gap-x-24">
+          <div className="lg:w-2/5">
+            <Image
+              src={productQuery.data.item.image}
+              alt=""
+              width={1000}
+              height={1000}
+              quality={75}
+              priority
+            />
+          </div>
+          <div className="flex-1">
+            <p className="lg:text-5xl font-bold text-primary">
+              {productQuery.data.item.product_name}
+            </p>
+          </div>
+        </section>
+      ) : (
+        <>No product found</>
+      )}
     </Layout>
   );
 }
