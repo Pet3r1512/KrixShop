da2d2bd5117d2009147e3423c7fed7debcc79b7f
Thanh Phong <pttp15122002@gmail.com>
2024-07-30 20:22:33 +1000

    Refactor: unnessary useState to store products

--- src/pages/shop/[...slug].tsx
+++ src/pages/shop/[...slug].tsx
@@ -5,7 +5,7 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useEffect, useState } from "react";
 import { Skeleton } from "@/components/UI/ui/skeleton";
 import { useRouter } from "next/router";
-import ProductCard, { Product } from "@/components/Shop/Product-card";
+import ProductCard from "@/components/Shop/Product-card";
 import EmptyProduct from "@/components/Shop/Empty-product";
 import Link from "next/link";
 
@@ -15,7 +15,6 @@ export type Params = {
 };
 
 export default function ProductPage() {
-  const [products, setProducts] = useState<any[]>([]);
   const [params, setParams] = useState<Params>({
     category: "",
     type: "",
@@ -28,12 +27,6 @@ export default function ProductPage() {
   });
 
   useEffect(() => {
-    if (productsQuery.isSuccess) {
-      setProducts(productsQuery.data?.products!);
-    }
-  }, [productsQuery]);
-
-  useEffect(() => {
     setParams({
       category: router.asPath.split("/")[2],
       type: router.asPath.split("/")[3],
@@ -42,7 +35,9 @@ export default function ProductPage() {
 
   return (
     <Layout pageName="Shop">
-      {productsQuery.isSuccess && products.length === 0 && <EmptyProduct />}
+      {productsQuery.isSuccess && productsQuery.data.products.length === 0 && (
+        <EmptyProduct />
+      )}
       <section className="grid md:grid-cols-3 lg:grid-cols-4 gap-y-3.5 grid-cols-2 justify-center lg:py-16 py-4">
         {productsQuery.isLoading && !productsQuery.isError && (
           <>
@@ -52,7 +47,7 @@ export default function ProductPage() {
           </>
         )}
         {productsQuery.isSuccess &&
-          products.map((product: Product) => {
+          productsQuery.data.products.map((product) => {
             return (
               <Link
                 key={product.xata_id}
