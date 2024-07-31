62fa4fa64db69475aeeaf56edb335f82a77c9c34
Thanh Phong <pttp15122002@gmail.com>
2024-07-31 10:52:39 +1000

    Fix: missing keys

--- src/pages/shop/item/[...slug].tsx
+++ src/pages/shop/item/[...slug].tsx
@@ -7,22 +7,40 @@ import Image from "next/image";
 import { cn, formatCurrency } from "@/lib/utils";
 import SaleoffBadge from "@/components/Shop/SaleoffBadge";
 import ItemSkeleton from "@/components/Shop/Item/item-skeleton";
+import { useTranslation } from "next-i18next";
 
 export default function ItemDetail() {
   const router = useRouter();
+  const { t } = useTranslation("common");
 
   const productQuery = trpc.product.getProductById.useQuery({
     xid: router.asPath.split("/")[3].toString(),
   });
 
-  return (
-    <Layout pageName="Shop">
-      {!productQuery.isLoading && !productQuery.data ? (
+  if (productQuery.isLoading) {
+    return (
+      <Layout pageName="Shop">
+        <ItemSkeleton />
+      </Layout>
+    );
+  }
+
+  if (!productQuery.isLoading && !productQuery.data) {
+    return (
+      <Layout pageName="Shop">
         <p className="text-xl font-semibold mt-16">Product is not existed!!!</p>
-      ) : productQuery.isSuccess && productQuery.data ? (
+      </Layout>
+    );
+  }
+
+  if (productQuery.data && productQuery.isSuccess) {
+    const product = productQuery.data.item;
+
+    return (
+      <Layout pageName="Shop">
         <section className="lg:my-16 my-8 md:flex md:flex-row lg:gap-x-24 px-4 lg:px-0">
           <Image
-            src={productQuery.data.item.image}
+            src={product.image}
             alt=""
             width={1000}
             height={1000}
@@ -33,56 +51,56 @@ export default function ItemDetail() {
           <div className="flex-1 flex flex-col gap-y-6 lg:gap-y-14">
             <div className="flex justify-between items-center relative gap-x-1">
               <p className="lg:text-5xl text-2xl font-bold text-primary flex-1 text-wrap">
-                {productQuery.data.item.product_name}
+                {product.product_name}
               </p>
               <div className="flex items-center gap-x-2 w-fit">
-                {productQuery.data.item.saleoff && (
+                {product.saleoff && (
                   <SaleoffBadge
                     className="size-10 text-sm"
-                    saleoff={productQuery.data.item.saleoff}
+                    saleoff={product.saleoff}
                   />
                 )}
-                {productQuery.data.item.quantity > 0 && <InStockBadge />}
-                {productQuery.data.item.quantity <= 0 && <OutStockBadge />}
+                {product.quantity > 0 && <InStockBadge />}
+                {product.quantity <= 0 && <OutStockBadge />}
               </div>
             </div>
             <div className="flex items-center">
               <p
                 className={cn(
-                  !productQuery.data.item.saleoff
+                  !product.saleoff
                     ? "hidden"
                     : "text-black mr-4 text-xl lg:text-2xl font-bold"
                 )}
               >
                 {formatCurrency(
                   (
-                    productQuery.data.item.price -
-                    (productQuery.data.item.price *
-                      productQuery.data.item.saleoff!) /
-                      100
+                    product.price -
+                    (product.price * product.saleoff!) / 100
                   ).toString()
                 )}
               </p>
               <p
                 className={cn(
                   `lg:text-2xl font-bold`,
-                  productQuery.data.item.saleoff &&
-                    "text-slate-300 line-through lg:text-xl"
+                  product.saleoff && "text-slate-300 line-through lg:text-xl"
                 )}
               >
-                {formatCurrency(productQuery.data.item.price.toString())}
+                {formatCurrency(product.price.toString())}
               </p>
             </div>
-            <p className="lg:text-xl">{productQuery.data.item.description}</p>
+            <p className="lg:text-xl">{product.description}</p>
             <div className="flex flex-col justify-center gap-y-2">
-              <p className="text-lg lg:text-xl font-semibold">Color</p>
+              <p className="text-lg lg:text-xl font-semibold">
+                {t("shop_page.colors")}
+              </p>
               <div className="flex items-center gap-x-3">
-                {productQuery.data.item.color.map((color) => {
+                {product.color.map((color) => {
                   return (
                     <div
                       style={{
                         backgroundColor: color,
                       }}
+                      key={color}
                       className={`size-10 rounded-lg ${
                         color === "#fff" ||
                         (color === "#FFF" && "border-2 border-black")
@@ -93,23 +111,31 @@ export default function ItemDetail() {
               </div>
             </div>
             <div className="flex flex-col justify-center gap-y-2">
-              <p className="text-lg lg:text-xl font-semibold">Size</p>
+              <p className="text-lg lg:text-xl font-semibold">
+                {t("shop_page.sizes")}
+              </p>
               <div className="flex items-center gap-x-3">
-                {productQuery.data.item.clothes_size &&
-                  productQuery.data.item.clothes_size.split(",").map((size) => {
+                {product.clothes_size &&
+                  product.clothes_size.split(",").map((size) => {
                     return (
-                      <div className="flex items-center justify-center size-10 border-2 border-black rounded-lg font-semibold">
+                      <div
+                        key={size}
+                        className="flex items-center justify-center size-10 border-2 border-black rounded-lg font-semibold"
+                      >
                         {size}
                       </div>
                     );
                   })}
-                {productQuery.data.item.footwear_size &&
-                  productQuery.data.item.footwear_size
+                {product.footwear_size &&
+                  product.footwear_size
                     .toString()
                     .split(",")
                     .map((size) => {
                       return (
-                        <div className="flex items-center justify-center size-10 border-2 border-black rounded-lg font-semibold">
+                        <div
+                          key={size}
+                          className="flex items-center justify-center size-10 border-2 border-black rounded-lg font-semibold"
+                        >
                           {size}
                         </div>
                       );
@@ -118,25 +144,25 @@ export default function ItemDetail() {
             </div>
           </div>
         </section>
-      ) : (
-        <ItemSkeleton />
-      )}
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
 
 function InStockBadge() {
+  const { t } = useTranslation("common");
   return (
     <button className="px-2.5 py-1.5 rounded-xl text-green-500 bg-green-100 font-semibold cursor-default">
-      In Stock
+      {t("shop_page.in_stock")}
     </button>
   );
 }
 
 function OutStockBadge() {
+  const { t } = useTranslation("common");
   return (
     <button className="px-2.5 py-1.5 rounded-xl bg-red-100 text-red-600 font-semibold cursor-default">
-      Run Out
+      {t("shop_page.out_stock")}
     </button>
   );
 }
