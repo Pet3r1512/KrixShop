9af08808123fc389464c4e4889ca8b7ee5814d7d
Thanh Phong <pttp15122002@gmail.com>
2024-07-31 10:24:21 +1000

    Refactor: display "run out" if needed

--- src/components/Shop/SaleoffBadge.tsx
+++ src/components/Shop/SaleoffBadge.tsx
@@ -3,18 +3,20 @@ import { cn } from "@/lib/utils";
 export default function SaleoffBadge({
   saleoff,
   className,
+  isOutStock,
 }: {
   saleoff: number;
   className?: string;
+  isOutStock?: boolean;
 }) {
   return (
     <div
       className={cn(
-        "rounded-full lg:size-16 lg:text-[24px] font-bold flex items-center justify-center bg-red-600 text-white",
+        "rounded-full lg:size-16 lg:text-[20px] font-bold flex text-wrap items-center text-center justify-center bg-red-600 text-white",
         className
       )}
     >
-      {saleoff}%
+      {isOutStock ? "Run Out" : `${saleoff}%`}
     </div>
   );
 }
