156a3305fabc891133a08e408c45a64351755d39
Thanh Phong <pttp15122002@gmail.com>
2024-07-31 16:03:25 +1000

    Style: add padding on mibiles

--- src/components/Shop/Empty-product.tsx
+++ src/components/Shop/Empty-product.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function EmptyProduct() {
   return (
-    <div className="lg:my-16">
+    <div className="lg:my-16 px-4 lg:px-0">
       <p className="lg:text-3xl font-bold">
         Sorry, there are no products available in this category at the moment.
       </p>
