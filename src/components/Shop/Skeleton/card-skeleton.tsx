f2c9d6a8ed77fcd00c4be3d472a6053d768b01fc
Thanh Phong <pttp15122002@gmail.com>
2024-07-31 12:46:19 +1000

    Update:  folder's name

---
+++ src/components/Shop/Skeleton/card-skeleton.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "@/components/UI/ui/skeleton";
+
+export default function CardSkeleton() {
+  return (
+    <Skeleton className="lg:w-64 w-40 md:w-56 lg:h-[392.75px] h-[275.84px] md:h-[349.97px] mx-auto lg:mx-0" />
+  );
+}
