aa0899d197581382c7c25d3cb3c81207ecb700ac
Thanh Phong <pttp15122002@gmail.com>
2024-08-04 11:34:36 +1000

    Feat: checkout page

---
+++ src/pages/checkout.tsx
@@ -0,0 +1,27 @@
+import CartItem from "@/components/Cart/CartItem";
+import ItemsTable from "@/components/Cart/ItemsTable";
+import Layout from "@/components/UI/Layout";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+
+export default function Checkout() {
+  return (
+    <Layout pageName="Checkout">
+      <main className="lg:my-8 my-4 min-h-screen lg:min-h-0 px-4 lg:px-0">
+        <p className="text-2xl lg:text-4xl font-semibold">Checkout</p>
+        <section className="flex flex-col lg:flex-row lg:items-center gap-x-2.5 mt-6">
+          {/* Cart items */}
+          <ItemsTable />
+          {/* Money check */}
+          <div className="lg:w-[35%]"></div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
+  props: {
+    ...(await serverSideTranslations(locale as string, ["common"])),
+  },
+});
