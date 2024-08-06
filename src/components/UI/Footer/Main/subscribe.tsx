31b00e621ed8a127d48895d6704108012f4a1fa4
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-08-06 12:19:11 +1000

    Style: update lang-change (#24)

--- src/components/UI/Footer/Main/subscribe.tsx
+++ src/components/UI/Footer/Main/subscribe.tsx
@@ -1,3 +1,4 @@
+import LangChange from "../../Header/Lang-change";
 import { PlaceholdersAndVanishInput } from "../../ui/aceternity/placeholders-and-vanish-input";
 
 export default function Subscribe() {
@@ -13,19 +14,22 @@ export default function Subscribe() {
   };
 
   return (
-    <div className="lg:w-1/3 flex flex-col gap-y-3">
-      <p className="font-bold text-xl">Subscribe</p>
-      <p>
-        Enter your email to be the first to know about new collections and
-        product launches.
-      </p>
-      <div className="size-fit bg-white rounded-full">
-        <PlaceholdersAndVanishInput
-          placeholders={placeholders}
-          onChange={handleChange}
-          onSubmit={onSubmit}
-        />
+    <div className="sm:w-full lg:w-1/3 flex flex-col sm:flex-row lg:flex-col gap-y-5 justify-between relative">
+      <div className="flex flex-col gap-y-3.5">
+        <p className="font-bold text-xl">Subscribe</p>
+        <p>
+          Enter your email to be the first to know about new collections and
+          product launches.
+        </p>
+        <div className="size-fit bg-white rounded-full">
+          <PlaceholdersAndVanishInput
+            placeholders={placeholders}
+            onChange={handleChange}
+            onSubmit={onSubmit}
+          />
+        </div>
       </div>
+      <LangChange />
     </div>
   );
 }
