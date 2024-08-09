8296a2c7a1c85a3cca6129c8badca8e2c99908a2
Thanh Phong <pttp15122002@gmail.com>
2024-08-09 14:52:49 +1000

    Feat: use province router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../tRPC";
 import { productRouter } from "./product";
+import { provinceRouter } from "./province_api";
 
 export const appRouter = router({
   product: productRouter,
+  province: provinceRouter,
 });
 
 export type AppRouter = typeof appRouter;
