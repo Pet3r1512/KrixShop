ca0232ed0b84e905192d157f7d96977cf5aa086e
Thanh Phong <pttp15122002@gmail.com>
2024-08-12 14:50:59 +1000

    Feat: validation address's info

--- src/components/Cart/Order/OrderSummary.tsx
+++ src/components/Cart/Order/OrderSummary.tsx
@@ -4,9 +4,16 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/UI/ui/accordion";
+import { Button } from "@/components/UI/ui/button";
+import { toast } from "@/components/UI/ui/use-toast";
+import { useCart } from "@/lib/hooks/useCart";
+import { formatCurrency } from "@/lib/utils";
 import { Address } from "@/pages/shipment";
+import { useRouter } from "next/router";
 
 export default function OrderSummary({ address }: { address: Address }) {
+  const router = useRouter();
+  const { getCurrentOrder } = useCart();
   return (
     <section className="flex-1">
       <p className="lg:text-2xl font-bold">Order Summary</p>
@@ -15,10 +22,16 @@ export default function OrderSummary({ address }: { address: Address }) {
           <AccordionTrigger>{"Cart's Info"}</AccordionTrigger>
           <AccordionContent className="lg:text-lg font-normal">
             <p>
-              Number of items: <span>4</span>
+              Items:{" "}
+              <span className="font-bold">
+                {getCurrentOrder().NumberOfItems}
+              </span>
             </p>
             <p>
-              Subtotal: <span>500.000</span>
+              Subtotal:{" "}
+              <span className="font-bold">
+                {formatCurrency(getCurrentOrder().subtotal.toString())}
+              </span>
             </p>
           </AccordionContent>
         </AccordionItem>
@@ -31,6 +44,7 @@ export default function OrderSummary({ address }: { address: Address }) {
               <span>{address.ward ? address.ward.split("|")[0] : ""}</span>
             </p>
             <p>{address.street ? address.street : ""}</p>
+            <p>{address.note ? address.note : ""}</p>
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="payment">
@@ -42,6 +56,24 @@ export default function OrderSummary({ address }: { address: Address }) {
           </AccordionContent>
         </AccordionItem>
       </Accordion>
+      <Button
+        onClick={() => {
+          address.district === "" ||
+          address.province === "" ||
+          address.ward === "" ||
+          address.street === ""
+            ? toast({
+                title: "Please Select All Fields",
+                duration: 1500,
+                className:
+                  "bg-[#fcbf49] text-white fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-2 sm:right-2 sm:top-auto sm:flex-col md:max-w-[420px] rounded-xl",
+              })
+            : router.push("/payment");
+        }}
+        className="w-full"
+      >
+        Next Step
+      </Button>
     </section>
   );
 }
