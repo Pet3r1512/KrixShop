3b883f8b19ec210b32173833565e8142d725f497
Thanh Phong <pttp15122002@gmail.com>
2024-08-14 15:14:43 +1000

    Feat: build payment page

--- src/pages/payment.tsx
+++ src/pages/payment.tsx
@@ -1,17 +1,27 @@
 import OrderSummary from "@/components/Cart/Order/OrderSummary";
+import Cash from "@/components/Cart/Payment/Instruction/Cash";
+import PaymentMethods from "@/components/Cart/Payment/PaymentMethods";
 import Layout from "@/components/UI/Layout";
 import { useAddress } from "@/lib/hooks/useAddress";
+import { useCart } from "@/lib/hooks/useCart";
 import { GetServerSideProps } from "next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-const payments = ["Cash", "Transfer", "Momo", "Cards"];
+export type PayMethod = "COD" | "Cards";
 
 const Payment = () => {
+  const [payMethod, setPayMethod] = useState<PayMethod>("COD");
   const { getAddress } = useAddress();
 
   const router = useRouter();
+  const { getCurrentOrder } = useCart();
+
+  const PayMethodInstructionView: Record<PayMethod, JSX.Element> = {
+    COD: <Cash subtotal={getCurrentOrder().subtotal} />,
+    Cards: <></>,
+  };
 
   useEffect(() => {
     if (
@@ -26,12 +36,17 @@ const Payment = () => {
 
   return (
     <Layout pageName="Payment">
-      <main className="lg:my-8 my-4 min-h-screen lg:min-h-0 px-4 lg:px-0 flex">
-        <p className="text-2xl lg:text-4xl font-semibold">Payment</p>
-        <section className="flex flex-col gap-y-4 w-full pr-5 h-full"></section>
-        <div className="w-full lg:w-[25%] self-start mt-24 lg:mt-0">
-          <OrderSummary />
-        </div>
+      <main className="lg:my-8 my-4 min-h-screen lg:min-h-0 px-4 lg:px-0">
+        <p className="text-2xl lg:text-4xl font-semibold">Pay Method</p>
+        <section className="flex flex-col lg:flex-row gap-x-10 mt-6">
+          <div className="w-full lg:my-8 flex flex-col gap-y-8 min-h-full">
+            <PaymentMethods setPayMethod={setPayMethod} />
+            {PayMethodInstructionView[payMethod]}
+          </div>
+          <div className="w-full lg:w-[25%] self-start mt-24 lg:mt-0">
+            <OrderSummary payMethod={payMethod} />
+          </div>
+        </section>
       </main>
     </Layout>
   );
