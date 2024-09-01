7789e8b9f872f8269cdc39b256a8c1f1d3e6aa14
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-09-01 14:40:15 +0700

    Build loading state for address inputs (#30)
    * Feat: update api service
    
    * Refactor: loading state for address inputs

--- src/server/routers/address.ts
+++ src/server/routers/address.ts
@@ -3,36 +3,36 @@ import { z } from "zod";
 
 export const addressRouter = router({
   getProvinces: publicProcedure.query(async () => {
-    const res = await fetch("https://open.oapi.vn/location/provinces?size=63", {
+    const res = await fetch("https://vapi.vnappmob.com/api/province", {
       method: "GET",
     });
 
     const data = await res.json();
 
-    return data.data;
+    return data;
   }),
   getDistricts: publicProcedure
-    .input(z.object({ province_code: z.string() }))
+    .input(z.object({ province_id: z.string() }))
     .query(async ({ input }) => {
-      const { province_code } = input;
+      const { province_id } = input;
       const res = await fetch(
-        "https://open.oapi.vn/location/districts?provinceId=" + province_code
+        "https://vapi.vnappmob.com/api/province/district/" + province_id
       );
 
       const data = await res.json();
 
-      return data.data;
+      return data;
     }),
   getWards: publicProcedure
-    .input(z.object({ district_code: z.string() }))
+    .input(z.object({ district_id: z.string() }))
     .query(async ({ input }) => {
-      const { district_code } = input;
+      const { district_id } = input;
       const res = await fetch(
-        "https://open.oapi.vn/location/wards?districtId= " + district_code
+        "https://vapi.vnappmob.com/api/province/ward/" + district_id
       );
 
       const data = await res.json();
 
-      return data.data;
+      return data;
     }),
 });
